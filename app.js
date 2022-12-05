96e5a4e0e280beb7405644dba75416468dd8224d
akilanselvam <selvamakilan1@gmail.com>
2022-12-05 14:52:03 +0530

    Completetion of Design Api

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const userRouters = require("./Routes/UserRouter");
 const contentRouters = require("./Routes/ContentRouter");
 
 app.use(express.json());
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.static(`${__dirname}/public`));
 app.get("/", (req, res) => {
   res.status(200).json({
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`your app is running under ${port}🍷🍷🍷.....!!`);
 });
